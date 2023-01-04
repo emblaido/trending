9ccb6ebecc4f1b90a01ec8f2aea16db5878b7f4f
camaya21 <cindymamaya21@gmail.com>
2023-01-04 10:29:21 -0500

    Brenda and Cindy's liveshare book & fashion js completed

---
+++ backend/controllers/fashion_controller.js
@@ -0,0 +1,3 @@
+const { json } = require('express')
+const express = require('express')
+const router = express.Router()
---
+++ backend/controllers/word_controller.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const router = express.Router()
+
+// Model import - goes here 
+
+router.use(express.json())
+
+
+//Static route 
+
+router.get('/awesome', (req, res, next) => {
+        res.json({message: 'Products are awesome!'})
+})
+
+module.exports = router
