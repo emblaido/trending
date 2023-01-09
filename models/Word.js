c0e5c087f3a39c39753f3335f40c36ef4358dd88
Emma Blaido <emmablaido@Emmas-Air.lan>
2023-01-09 14:44:18 -0600

    word.js and book.js updated

--- models/Word.js
+++ models/Word.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
  
 const WordSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    title: String,
+    name: {type: String, require: [true, "name cannot be empty"]},
+    description: {type: String, require: [true, "description cannot be empty"]},
+    link: {type: String},
 },{timestamp: true});
 
 const Word = mongoose.model('Word', WordSchema);
