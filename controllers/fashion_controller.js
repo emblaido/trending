0646853ee5fa269a00aae645c1b8d7bcc2a63980
bavila19 <brendaavila015@gmail.com>
2023-01-10 13:01:59 -0700

    This code works. Pushing it up for good measure.

--- controllers/fashion_controller.js
+++ controllers/fashion_controller.js
@@ -1,59 +1,63 @@
-const { json } = require('express')
-const express = require('express')
-const router = express.Router()
-router.use(express.json())
-const {Fashion} = require('../models')
+const { json } = require("express");
+const express = require("express");
+const router = express.Router();
+router.use(express.json());
+const { Fashion } = require("../models");
 //const db = require('../models')
 
 //Index route
-router.get('/', async (req,res)=>{ 
-    try {
-        const fashionista = await Fashion.find({})
-        return res.status(200).json(fashionista)
-    } catch(error) {
-        console.error(error)
-        return next(error)
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const fashionista = await Fashion.find({});
+    return res.status(200).json(fashionista);
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
 
 //Show Route
-router.get('/:id', async(req,res)=>{
-    try{
-        const findFashion = await Fashion.findById(req.params.id)
-        res.status(201).json(findFashion)
-    }catch(err){
-        res.status(400).json({error: err.message})
-    }
-})
+router.get("/:id", async (req, res) => {
+  try {
+    const findFashion = await Fashion.findById(req.params.id);
+    res.status(201).json(findFashion);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
 
 //Post Route
-router.post('/', async(req, res)=>{
-    try{
-        const newFashion = await Fashion.create(req.body)
-        res.status(201).json(newFashion)
-    }catch(err){
-        res.status(400).json ({error:err.message})
-    }
-})
+router.post("/", async (req, res) => {
+  try {
+    const newFashion = await Fashion.create(req.body);
+    res.status(201).json(newFashion);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
 
 //Update Route
-router.put('/:id', async(req,res, next)=>{
-    try{
-        const updatedFashion = await Fashion.findByIdAndUpdate(req.params.id, req.body, {new: true})
-        res.status(201).json(updatedFashion)
-    }catch(err){
-        res.status(400).json ({error: err.message})
-    }
-})
+router.put("/:id", async (req, res, next) => {
+  try {
+    const updatedFashion = await Fashion.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(201).json(updatedFashion);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
 
 // Delete Route
-router.delete('/:id', async(req,res)=>{
-    try{
-        const deletedFashion = await Fashion.findByIdAndDelete(req.params.id)
-        res.status(201).json(deletedFashion)
-    }catch(err){
-        res.status(400).json({error: err.message})
-    }
-})
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedFashion = await Fashion.findByIdAndDelete(req.params.id);
+    res.status(201).json(deletedFashion);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
 
-module.exports = router
+module.exports = router;
